4a9ab383422eb2ecb1c4fd46c306eb4458a0892d
khk <jasun1208@naver.com>
2021-09-13 23:23:57 +0900

    기본 setting및 db연결

---
+++ instagram/instagram/users/views.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
+def main(request):
+    if request.method == 'GET':
+        return render(request, 'users/main.html')
+    elif request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('posts:index'))
+
+            else:
+                return render(request, 'users/main.html')
